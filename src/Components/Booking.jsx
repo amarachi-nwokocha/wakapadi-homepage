f5a226fceaafc700a4895842540804234473b232
amarachi-nwokocha <63500921+amarachi-nwokocha@users.noreply.github.com>
2023-11-16 02:57:42 +0100

    chnages madeto booking component

--- src/Components/Booking.jsx
+++ src/Components/Booking.jsx
@@ -1,13 +1,13 @@
 const Booking = () => {
   return (
-    <div id="deals" className="max-w-[1140px] p-4 m-auto w-full">
+    <div id="deals" className="max-w-[1140px] p-4 m-auto w-full md:mt-[90px]">
       <form className="lg:flex lg:justify-between w-full item-center">
         <div className="flex flex-col my-2 py-2">
           <label>Destination</label>
           <select className="lg:w-[300px] md:w-full border rounded-md p-2 ">
-            <option value="">something1</option>
-            <option value="">something2</option>
-            <option value="">something3</option>
+            <option value="">Asia</option>
+            <option value="">Caribbeans</option>
+            <option value="">Middle East</option>
           </select>
         </div>
         <div className="flex flex-col md:flex-row w-full ">
